a86648df5b076a9347f4c80b906f6222acf69435
paolaMedina <angie.paola.medina@correounivalle.edu.co>
2019-04-02 02:57:19 +0000

    null true en abreviatura colector

--- cientifico/models.py
+++ cientifico/models.py
@@ -5,5 +5,5 @@ from django.db import models
 # Create your models here.
 class Cientifico(models.Model):
     nombre_completo = models.CharField(max_length=200)
-    nombre_abreviado = models.CharField(blank=True, default=None, max_length=200)
+    nombre_abreviado = models.CharField(blank=True,  null=True,default=None, max_length=200)
     unique_together  =  ( "nombre_completo" ,  "nombre_abreviado" )
