08767f086be2f1fa32bd236a8d0613b9afdff978
paolaMedina <angie.paola.medina@correounivalle.edu.co>
2019-07-25 09:32:45 -0500

    aprobacion de citas

--- visita/urls.py
+++ visita/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from . import views
-from .views import RegistroVisita,ListarVisitas,aprobar
+from .views import *
 app_name = 'visita'
 urlpatterns = [
-    
+
     path(r'generar', RegistroVisita.as_view(), name='crear_visita'),
     path(r'ver/<int:pk>', views.view, name='detalle_visita'),
     path(r'listar', ListarVisitas.as_view(), name='listar_visita'),
     path(r'aprobar/<int:pk>', views.aprobar, name='aprobar_visita'),
+    path(r'envioCorreo', views.envioCorreo, name='envio_correo'),
     # path(r'deleteFile', views.EliminarArchivo, name='eliminar_archivo'),
 ]
